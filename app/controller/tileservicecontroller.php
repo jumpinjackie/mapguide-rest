cb1a4a2f848bb58a1ee6c6ad44c31dbf4717272b
Jackie Ng <jumpinjackie@gmail.com>
2014-05-24 18:05:24 +1000

    #52, #53: Implement caching and locking for XYZ tiles. Also fix bad tile storage path.

--- app/controller/tileservicecontroller.php
+++ app/controller/tileservicecontroller.php
@@ -102,6 +102,7 @@ class MgTileServiceController extends MgBaseController {
                 $app->lastModified($tmd);
             }
             $app->expires("+6 months");
+            $app->response->header("Cache-Control", "max-age=31536000, must-revalidate");
         }, true, "", $sessionId); //Tile access can be anonymous, so allow for it if credentials/session specified, but if this is a session-based Map Definition, use the session id as the nominated one
     }
 
@@ -157,7 +158,7 @@ class MgTileServiceController extends MgBaseController {
     }
 
     private static function GetTilePath($app, $resId, $groupName, $z, $x, $y, $type) {
-        $relPath = "/".$resId->GetPath()."/".$resId->GetName()."/$groupName/$x/$x/$y.$type";
+        $relPath = "/".$resId->GetPath()."/".$resId->GetName()."/$groupName/$z/$x/$y.$type";
         $path = $app->config("AppRootDir")."/".$app->config("Cache.RootDir")."/tile.$type".$relPath;
         return $path;
     }
@@ -331,118 +332,184 @@ class MgTileServiceController extends MgBaseController {
         $fmt = $this->ValidateRepresentation($type, array("json", "png", "png8", "jpg", "gif"));
 
         $path = self::GetTilePath($this->app, $resId, $groupName, $z, $x, $y, $type);
-        if (file_exists($path)) {
-            $this->app->lastModified(filemtime($path));
-        } else {
-            $dir = dirname($path);
-            if (!is_dir($dir)) {
-                try {
-                    mkdir($dir, 0777, true);
-                } catch (Exception $e) { //Another tile request may have already created this since
+        clearstatcache();
 
+        $dir = dirname($path);
+        if (!@is_dir($dir)) {
+            try {
+                mkdir($dir, 0777, true);
+            } catch (Exception $e) { //Another tile request may have already created this since
+
+            }
+        }
+        $lockPath = "$dir/lock.lck";
+        $fpLockFile = fopen($lockPath, "a+");
+
+        $requestId = rand();
+        error_log("($requestId) Checking if $path exists");
+
+        if (!file_exists($path)) {
+
+            error_log("($requestId) $path does not exist. Locking for writing");
+
+            $bLocked = false;
+            flock($fpLockFile, LOCK_EX);
+            fwrite($fpLockFile, ".");
+            $bLocked = true;
+
+            error_log("($requestId) Acquired lock for $path. Checking if path exists again.");
+
+            //check once more to see if the cache file was created while waiting for
+            //the lock
+            clearstatcache();
+            if (!file_exists($path)) {
+                try {
+                    error_log("($requestId) Rendering tile to $path");
+
+                    $this->EnsureAuthenticationForSite("", true);
+                    $siteConn = new MgSiteConnection();
+                    $siteConn->Open($this->userInfo);
+
+                    $map = new MgMap($siteConn);
+                    $map->Create($resId, "VectorTileMap");
+
+                    $layerGroups = $map->GetLayerGroups();
+                    $baseGroup = $layerGroups->GetItem($groupName);
+
+                    $factory = new MgCoordinateSystemFactory();
+                    $mapCsWkt = $map->GetMapSRS();
+                    $mapCs = $factory->Create($mapCsWkt);
+                    
+                    $mapExtent = $map->GetMapExtent();
+                    $mapExLL = $mapExtent->GetLowerLeftCoordinate();
+                    $mapExUR = $mapExtent->GetUpperRightCoordinate();
+
+                    $metersPerUnit = $mapCs->ConvertCoordinateSystemUnitsToMeters(1.0);
+
+                    error_log("($requestId) Calc bounds from XYZ");
+                    //XYZ to lat/lon math. From this we can convert to the bounds in the map's CS
+                    //
+                    //Source: http://wiki.openstreetmap.org/wiki/Slippy_map_tilenames
+                    $n = pow(2, $z);
+                    $lonMin = $x / $n * 360.0 - 180.0;
+                    $latMin = rad2deg(atan(sinh(pi() * (1 - 2 * $y / $n))));
+                    $lonMax = ($x + 1) / $n * 360.0 - 180.0;
+                    $latMax = rad2deg(atan(sinh(pi() * (1 - 2 * ($y + 1) / $n))));
+
+                    $boundsMinX = min($lonMin, $lonMax);
+                    $boundsMinY = min($latMin, $latMax);
+                    $boundsMaxX = max($lonMax, $lonMin);
+                    $boundsMaxY = max($latMax, $latMin);
+
+                    if ($mapCs->GetCsCode() != "LL84") {
+                        $llCs = $factory->CreateFromCode("LL84");
+                        $trans = $factory->GetTransform($llCs, $mapCs);
+
+                        $ul = $trans->Transform($lonMin, $latMin);
+                        $lr = $trans->Transform($lonMax, $latMax);
+
+                        $boundsMinX = min($lr->GetX(), $ul->GetX());
+                        $boundsMinY = min($lr->GetY(), $ul->GetY());
+                        $boundsMaxX = max($lr->GetX(), $ul->GetX());
+                        $boundsMaxY = max($lr->GetY(), $ul->GetY());
+                    }
+
+                    //Set all layers under group to be visible
+                    $layers = $map->GetLayers();
+                    $groups = $map->GetLayerGroups();
+                    $layerCount = $layers->GetCount();
+
+                    if ($groups->IndexOf($groupName) < 0) {
+                        throw new Exception("Group not found: $groupName"); //TODO: Localize
+                    } else {
+                        $grp = $groups->GetItem($groupName);
+                        $grp->SetVisible(true);
+                    }
+
+                    for ($i = 0; $i < $layerCount; $i++) {
+                        $layer = $layers->GetItem($i);
+                        $group = $layer->GetGroup();
+                        if (null == $group) {
+                            continue;
+                        }
+                        if ($group->GetName() != $groupName) {
+                            $layer->SetVisible(false);
+                            continue;
+                        }
+                        if ($layer->GetLayerType() == MgLayerType::Dynamic)
+                            $layer->SetVisible(true);
+                    }
+
+                    if ($type == "json") {
+                        error_log("($requestId) Render vector tile");
+                        $this->PutVectorTileXYZ($map, $groupName, $siteConn, $metersPerUnit, $factory, $path, $boundsMinX, $boundsMinY, $boundsMaxX, $boundsMaxY);
+                    } else {
+                        $format = strtoupper($type);
+                        error_log("($requestId) Render image tile");
+                        $this->PutTileImageXYZ($map, $groupName, $siteConn, $path, $format, $boundsMinX, $boundsMinY, $boundsMaxX, $boundsMaxY);
+                    }
+                } catch (MgException $ex) {
+                    if ($bLocked) {
+                        error_log("($requestId) MgException caught ".$ex->GetExceptionMessage().". Releasing lock for $path");
+                        flock($fpLockFile, LOCK_UN);
+                        $bLocked = false;
+                    }
+                    if ($ex instanceof MgResourceNotFoundException) {
+                        $this->app->halt(404, $ex->GetExceptionMessage());
+                    }
+                } catch (Exception $ex) {
+                    if ($bLocked) {
+                        error_log("($requestId) Exception caught. Releasing lock for $path");
+                        flock($fpLockFile, LOCK_UN);
+                        $bLocked = false;
+                    }
                 }
             }
-        }
 
-        $this->EnsureAuthenticationForSite("", true);
-        $siteConn = new MgSiteConnection();
-        $siteConn->Open($this->userInfo);
-
-        $map = new MgMap($siteConn);
-        $map->Create($resId, "VectorTileMap");
-
-        $layerGroups = $map->GetLayerGroups();
-        $baseGroup = $layerGroups->GetItem($groupName);
-
-        $factory = new MgCoordinateSystemFactory();
-        $mapCsWkt = $map->GetMapSRS();
-        $mapCs = $factory->Create($mapCsWkt);
-        
-        $mapExtent = $map->GetMapExtent();
-        $mapExLL = $mapExtent->GetLowerLeftCoordinate();
-        $mapExUR = $mapExtent->GetUpperRightCoordinate();
-
-        $metersPerUnit = $mapCs->ConvertCoordinateSystemUnitsToMeters(1.0);
-
-        //XYZ to lat/lon math. From this we can convert to the bounds in the map's CS
-        //
-        //Source: http://wiki.openstreetmap.org/wiki/Slippy_map_tilenames
-        $n = pow(2, $z);
-        $lonMin = $x / $n * 360.0 - 180.0;
-        $latMin = rad2deg(atan(sinh(pi() * (1 - 2 * $y / $n))));
-        $lonMax = ($x + 1) / $n * 360.0 - 180.0;
-        $latMax = rad2deg(atan(sinh(pi() * (1 - 2 * ($y + 1) / $n))));
-
-        $boundsMinX = min($lonMin, $lonMax);
-        $boundsMinY = min($latMin, $latMax);
-        $boundsMaxX = max($lonMax, $lonMin);
-        $boundsMaxY = max($latMax, $latMin);
-
-        if ($mapCs->GetCsCode() != "LL84") {
-            $llCs = $factory->CreateFromCode("LL84");
-            $trans = $factory->GetTransform($llCs, $mapCs);
-
-            $ul = $trans->Transform($lonMin, $latMin);
-            $lr = $trans->Transform($lonMax, $latMax);
-
-            $boundsMinX = min($lr->GetX(), $ul->GetX());
-            $boundsMinY = min($lr->GetY(), $ul->GetY());
-            $boundsMaxX = max($lr->GetX(), $ul->GetX());
-            $boundsMaxY = max($lr->GetY(), $ul->GetY());
-        }
-
-        //Set all layers under group to be visible
-        $layers = $map->GetLayers();
-        $groups = $map->GetLayerGroups();
-        $layerCount = $layers->GetCount();
-
-        if ($groups->IndexOf($groupName) < 0) {
-            throw new Exception("Group not found: $groupName"); //TODO: Localize
-        } else {
-            $grp = $groups->GetItem($groupName);
-            $grp->SetVisible(true);
-        }
-
-        for ($i = 0; $i < $layerCount; $i++) {
-            $layer = $layers->GetItem($i);
-            $group = $layer->GetGroup();
-            if (null == $group) {
-                continue;
+            if ($bLocked) {
+                error_log("($requestId) Releasing lock for $path");
+                flock($fpLockFile, LOCK_UN);
+                $bLocked = false;
             }
-            if ($group->GetName() != $groupName) {
-                $layer->SetVisible(false);
-                continue;
-            }
-            if ($layer->GetLayerType() == MgLayerType::Dynamic)
-                $layer->SetVisible(true);
         }
 
-        if ($type == "json") {
-            $this->PutVectorTileXYZ($map, $groupName, $siteConn, $metersPerUnit, $factory, $path, $boundsMinX, $boundsMinY, $boundsMaxX, $boundsMaxY);
-            $this->app->response->header("Content-Type", MgMimeType::Json);
-        } else {
-            $format = strtoupper($type);
-            $mimeType = "";
-            switch ($format) {
-                case "PNG": //MgImageFormats::Png:
-                    $mimeType = MgMimeType::Png;
-                    break;
-                case "PNG8": //MgImageFormats::Png8:
-                    $mimeType = MgMimeType::Png;
-                    break;
-                case "GIF": //MgImageFormats::Gif:
-                    $mimeType = MgMimeType::Gif;
-                    break;
-                case "JPG": //MgImageFormats::Jpeg:
-                    $mimeType = MgMimeType::Jpeg;
-                    break;
-            }
-            $this->PutTileImageXYZ($map, $groupName, $siteConn, $path, $format, $boundsMinX, $boundsMinY, $boundsMaxX, $boundsMaxY);
-            $this->app->response->header("Content-Type", $mimeType);
+        $modTime = filemtime($path);
+        $this->app->lastModified($modTime);
+
+        error_log("($requestId) Acquiring shared lock for $path");
+        //acquire shared lock for reading to prevent a problem that could occur
+        //if a tile exists but is only partially generated.
+        flock($fpLockFile, LOCK_SH);
+
+        error_log("($requestId) Outputting $path");
+
+        $ext = strtoupper(pathinfo($path, PATHINFO_EXTENSION));
+        $mimeType = "";
+        switch ($ext) {
+            case "PNG": //MgImageFormats::Png:
+                $mimeType = MgMimeType::Png;
+                break;
+            case "GIF": //MgImageFormats::Gif:
+                $mimeType = MgMimeType::Gif;
+                break;
+            case "JPG": //MgImageFormats::Jpeg:
+                $mimeType = MgMimeType::Jpeg;
+                break;
+            case "JSON":
+                $mimeType = MgMimeType::Json;
+                break;
         }
 
-        $this->app->lastModified(filemtime($path));
+        $this->app->response->header("Content-Type", $mimeType);
+        $this->app->expires("+6 months");
+        $this->app->response->header("Cache-Control", "max-age=31536000, must-revalidate");
         $this->app->response->setBody(file_get_contents($path));
+
+        error_log("($requestId) Releasing shared lock for $path");
+
+        //Release lock
+        flock($fpLockFile, LOCK_UN);
+        fclose($fpLockFile);
     }
 }
 
