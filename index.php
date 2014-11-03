973dfc1191964a9651e08e0b8387f3521f7bde1f
Jackie Ng <jumpinjackie@gmail.com>
2014-11-04 02:00:31 +1100

    #79: Namespace our REST API to v1. Also move our test suite into a v1 test suite, and add test runner support for executing tests in a particular namespace (currently only v1). This allows requests to be made to either:
     - /rest/*
     - /rest/v1/*
    
    The v1 routes will be set in stone (ensuring stability for client applications), whereas the un-versioned routes can be subject to change.

--- index.php
+++ index.php
@@ -82,15 +82,29 @@ include dirname(__FILE__)."/app/formatters/registration.php";
 //Set the root dir of this file for code that needs to know about it
 $app->config("AppRootDir", dirname(__FILE__));
 include "app/config.php";
-include "app/routes/routes.data.php";
-include "app/routes/routes.library.php";
-include "app/routes/routes.session.php";
-include "app/routes/routes.coordsys.php";
-include "app/routes/routes.providers.php";
-include "app/routes/routes.site.php";
-include "app/routes/routes.admin.php";
-include "app/routes/routes.services.php";
-include "app/routes/routes.doc.php";
+//Register these routes in the default namespace
+include "app/routes/routes.data.v1.php";
+include "app/routes/routes.library.v1.php";
+include "app/routes/routes.session.v1.php";
+include "app/routes/routes.coordsys.v1.php";
+include "app/routes/routes.providers.v1.php";
+include "app/routes/routes.site.v1.php";
+include "app/routes/routes.admin.v1.php";
+include "app/routes/routes.services.v1.php";
+include "app/routes/routes.doc.v1.php";
+//Now scope the same routes under the v1 namespace. While the above includes may change, the includes
+//here cannot. We have effectively set these routes "in stone". Once set, this cannot be changed
+$app->group("/v1", function() use ($app) {
+    include "app/routes/routes.data.v1.php";
+    include "app/routes/routes.library.v1.php";
+    include "app/routes/routes.session.v1.php";
+    include "app/routes/routes.coordsys.v1.php";
+    include "app/routes/routes.providers.v1.php";
+    include "app/routes/routes.site.v1.php";
+    include "app/routes/routes.admin.v1.php";
+    include "app/routes/routes.services.v1.php";
+    include "app/routes/routes.doc.v1.php";
+});
 /*
 $app->get("/", function() {
     echo "Hello World";
