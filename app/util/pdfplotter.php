jumpinjackie/mapguide-rest

commit 6f613d29add92dd4354ade3884a9ef1b1069ecaf

Don't lock layers in layered pdfs
tcpdf makes all layers locked by default. This means you can not
hide them in acrobat reader (tested with version XI). Thus rendering
the layered option useless.

With this fix layers are not locked by default, and layers show up
correctly in adobe acrobat.

app/util/pdfplotter.php
@@ -681,7 +681,10 @@ class MgPdfPlotter
                     $mapGroups->GetItem($prevGroupName)->SetVisible(false);
                 }
                 $mapGroups->GetItem($groupName)->SetVisible(true);
-                $this->pdf->startLayer($groupName);
+				$print = true;
+				$view = true;
+				$lock = false;
+                $this->pdf->startLayer($groupName,$print,$view,$lock);
 
                 $filelocation = $this->RenderMap(MgUtils::InToPx($this->printSize->width, $this->dpi),
                                                  MgUtils::InToPx($this->printSize->height, $this->dpi),
@@ -719,7 +722,10 @@ class MgPdfPlotter
                 }
                 $mapLayers->GetItem($layerName)->SetVisible(true);
                 
-                $this->pdf->startLayer($layerName);
+				$print = true;
+				$view = true;
+				$lock = false;
+                $this->pdf->startLayer($groupName,$print,$view,$lock);
 
                 $filelocation = $this->RenderMap(MgUtils::InToPx($this->printSize->width, $this->dpi),
                                                  MgUtils::InToPx($this->printSize->height, $this->dpi),

commit 0d846f50edcfac3b3b0a146962181129bd6888bf

PDF plotting fixes.  - Fix incorrect startLayer() call (I overlooked a problem introduced in 6f613d2)  - Re-introduce setting PDF mime type

app/util/pdfplotter.php
@@ -681,9 +681,9 @@ class MgPdfPlotter
                     $mapGroups->GetItem($prevGroupName)->SetVisible(false);
                 }
                 $mapGroups->GetItem($groupName)->SetVisible(true);
-				$print = true;
-				$view = true;
-				$lock = false;
+                $print = true;
+                $view = true;
+                $lock = false;
                 $this->pdf->startLayer($groupName,$print,$view,$lock);
 
                 $filelocation = $this->RenderMap(MgUtils::InToPx($this->printSize->width, $this->dpi),
@@ -722,10 +722,10 @@ class MgPdfPlotter
                 }
                 $mapLayers->GetItem($layerName)->SetVisible(true);
                 
-				$print = true;
-				$view = true;
-				$lock = false;
-                $this->pdf->startLayer($groupName,$print,$view,$lock);
+                $print = true;
+                $view = true;
+                $lock = false;
+                $this->pdf->startLayer($layerName,$print,$view,$lock);
 
                 $filelocation = $this->RenderMap(MgUtils::InToPx($this->printSize->width, $this->dpi),
                                                  MgUtils::InToPx($this->printSize->height, $this->dpi),
@@ -790,6 +790,7 @@ class MgPdfPlotter
             $mExt = $this->DrawExtentCS($legendWidthIn);
         }
         
+        $this->app->response->header("Content-Type", "application/pdf");
         $mode = 'I';
         $name = 'Map.pdf';
         if (strlen($this->title) > 0)