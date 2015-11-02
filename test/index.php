e1be54ead85aa5a8b5751baeb4efaab2e49d47e6
Jackie Ng <jumpinjackie@gmail.com>
2015-11-03 01:10:17 +1100

    Update QUnit to 1.20

--- test/index.php
+++ test/index.php
@@ -39,12 +39,12 @@ if ($dump) {
 <html>
     <head>
         <title>MapGuide REST API Test Runner</title>
-        <link rel="stylesheet" media="screen" href="qunit-1.10.0.css">
+        <link rel="stylesheet" media="screen" href="qunit-1.20.0.css">
         <script src="../assets/common/js/jquery-1.10.2.min.js" type="text/javascript"></script>
     </head>
     <body>
         <div id="qunit"></div>
-        <script src="qunit-1.10.0.js" type="text/javascript"></script>
+        <script src="qunit-1.20.0.js" type="text/javascript"></script>
         <script type="text/javascript">
 
             var MgMimeType = {
