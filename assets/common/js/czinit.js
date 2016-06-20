bcf98951fddd8571b11cd2aa785d6275415d1ffa
Jackie Ng <jumpinjackie@gmail.com>
2016-06-20 22:37:59 +1000

    Update cesium examples to default to OSM/Stamen instead of Bing Maps.
    Fixes #135

---
+++ assets/common/js/czinit.js
@@ -0,0 +1,31 @@
+// We're not using bing here. If using bing, please provide your own API key
+Cesium.BingMapsApi.defaultKey = null;
+
+function createViewer(target, scriptUrlRoot) {
+    //Create the list of available providers we would like the user to select from.
+    //This example uses 3, OpenStreetMap, The Black Marble, and a single, non-streaming world image.
+    var imageryViewModels = [
+        new Cesium.ProviderViewModel({
+            name: "OpenStreetMap",
+            tooltip: "OpenStreetMap",
+            iconUrl: scriptUrlRoot + "/Cesium/Widgets/Images/ImageryProviders/openStreetMap.png",
+            creationFunction: function () {
+                return new Cesium.OpenStreetMapImageryProvider();
+            }
+        }),
+        new Cesium.ProviderViewModel({
+            name: "Stamen Toner",
+            tooltip: "Stamen Toner",
+            iconUrl: scriptUrlRoot + "/Cesium/Widgets/Images/ImageryProviders/stamenToner.png",
+            creationFunction: function () {
+                return new Cesium.OpenStreetMapImageryProvider({
+                    url: "//stamen-tiles.a.ssl.fastly.net/toner/",
+                    credit: "Map tiles by Stamen Design, under CC BY 3.0. Data by OpenStreetMap, under CC BY SA"
+                });
+            }
+        })
+    ];
+
+    var viewer = new Cesium.Viewer(target, { imageryProviderViewModels: imageryViewModels });
+    return viewer;
+}
