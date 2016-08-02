0a5037f513aa9e13554c6ec7ae8ab1fba6dcb117
Jackie Ng <jumpinjackie@gmail.com>
2016-08-03 03:00:47 +1000

    #138: Port over EnumerateResourceData and JSON roundtrip tests

---
+++ integration_test/ResourceService/EnumerateResourceDataTest.php
@@ -0,0 +1,102 @@
+<?php
+
+//
+//  Copyright (C) 2016 by Jackie Ng
+//
+//  This library is free software; you can redistribute it and/or
+//  modify it under the terms of version 2.1 of the GNU Lesser
+//  General Public License as published by the Free Software Foundation.
+//
+//  This library is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//  Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public
+//  License along with this library; if not, write to the Free Software
+//  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+//
+
+require_once dirname(__FILE__)."/../Config.php";
+require_once dirname(__FILE__)."/../ServiceTest.php";
+
+class EnumerateResourceDataTest extends ServiceTest {
+    public function testCommon() {
+        $resp = $this->apiTest("/library/Samples/Sheboygan/Data/Parcels.FeatureSource/datalist", "POST", null);
+        $this->assertStatusCodeIs(404, $resp);
+
+        $resp = $this->apiTest("/library/Samples/Sheboygan/Data/Parcels.FeatureSource/datalist", "PUT", null);
+        $this->assertStatusCodeIs(404, $resp);
+
+        $resp = $this->apiTestAnon("/library/Samples/Sheboygan/Data/Parcels.FeatureSource/datalist.jsdhf", "GET", null);
+        $this->assertStatusCodeIs(400, $resp);
+        $this->assertMimeType(Configuration::MIME_HTML, $resp);
+
+        $resp = $this->apiTestAnon("/library/Samples/Sheboygan/Data/Parcels.FeatureSource/datalist.jsdhf", "GET", array("depth" => "-1", "type" => "LayerDefinition"));
+        $this->assertStatusCodeIs(400, $resp);
+        $this->assertMimeType(Configuration::MIME_HTML, $resp);
+
+        $resp = $this->apiTestAdmin("/library/Samples/Sheboygan/Data/Parcels.FeatureSource/datalist.jsdhf", "GET", null);
+        $this->assertStatusCodeIs(400, $resp);
+        $this->assertMimeType(Configuration::MIME_HTML, $resp);
+
+        $resp = $this->apiTestAdmin("/library/Samples/Sheboygan/Data/Parcels.FeatureSource/datalist.jsdhf", "GET", array("depth" => "-1", "type" => "LayerDefinition"));
+        $this->assertStatusCodeIs(400, $resp);
+        $this->assertMimeType(Configuration::MIME_HTML, $resp);
+    }
+    private function __testBase($extension, $mimeType) {
+        //Bad credentials
+        $resp = $this->apiTestWithCredentials("/library/Samples/Sheboygan/Data/Parcels.FeatureSource/datalist.$extension", "GET", null, "Foo", "Bar");
+        $this->assertStatusCodeIs(401, $resp);
+        $this->assertMimeType($mimeType, $resp);
+
+        $resp = $this->apiTestWithCredentials("/library/Samples/Sheboygan/Data/Parcels.FeatureSource/datalist.$extension", "GET", array("depth" => "-1", "type" => "LayerDefinition"), "Foo", "Bar");
+        $this->assertStatusCodeIs(401, $resp);
+        $this->assertMimeType($mimeType, $resp);
+
+        //Raw credentials
+        $resp = $this->apiTestAnon("/library/Samples/Sheboygan/Data/Parcels.FeatureSource/datalist.$extension", "GET", null);
+        $this->assertStatusCodeIs(200, $resp);
+        $this->assertMimeType($mimeType, $resp);
+
+        $resp = $this->apiTestAnon("/library/Samples/Sheboygan/Data/Parcels.FeatureSource/datalist.$extension", "GET", array("depth" => "-1", "type" => "LayerDefinition"));
+        $this->assertStatusCodeIs(200, $resp);
+        $this->assertMimeType($mimeType, $resp);
+
+        $resp = $this->apiTestAdmin("/library/Samples/Sheboygan/Data/Parcels.FeatureSource/datalist.$extension", "GET", null);
+        $this->assertStatusCodeIs(200, $resp);
+        $this->assertMimeType($mimeType, $resp);
+
+        $resp = $this->apiTestAdmin("/library/Samples/Sheboygan/Data/Parcels.FeatureSource/datalist.$extension", "GET", array("depth" => "-1", "type" => "LayerDefinition"));
+        $this->assertStatusCodeIs(200, $resp);
+        $this->assertMimeType($mimeType, $resp);
+
+        //Session ID
+        $resp = $this->apiTest("/library/Samples/Sheboygan/Data/Parcels.FeatureSource/datalist.$extension", "GET", array("session" => $this->anonymousSessionId));
+        $this->assertStatusCodeIs(200, $resp);
+        $this->assertMimeType($mimeType, $resp);
+
+        $resp = $this->apiTest("/library/Samples/Sheboygan/Data/Parcels.FeatureSource/datalist.$extension", "GET", array("session" => $this->anonymousSessionId, "depth" => "-1", "type" => "LayerDefinition"));
+        $this->assertStatusCodeIs(200, $resp);
+        $this->assertMimeType($mimeType, $resp);
+
+        $resp = $this->apiTest("/library/Samples/Sheboygan/Data/Parcels.FeatureSource/datalist.$extension", "GET", array("session" => $this->adminSessionId));
+        $this->assertStatusCodeIs(200, $resp);
+        $this->assertMimeType($mimeType, $resp);
+
+        $resp = $this->apiTest("/library/Samples/Sheboygan/Data/Parcels.FeatureSource/datalist.$extension", "GET", array("session" => $this->adminSessionId, "depth" => "-1", "type" => "LayerDefinition"));
+        $this->assertStatusCodeIs(200, $resp);
+        $this->assertMimeType($mimeType, $resp);
+    }
+    public function testXml() {
+        $this->__testBase("xml", Configuration::MIME_XML);
+    }
+    public function testJson() {
+        $this->__testBase("json", Configuration::MIME_JSON);
+    }
+    public function testHtml() {
+        $this->__testBase("html", Configuration::MIME_HTML);
+    }
+}
+
+?>
---
+++ integration_test/ResourceService/JSONRoundtripTest.php
@@ -0,0 +1,34 @@
+<?php
+
+//
+//  Copyright (C) 2016 by Jackie Ng
+//
+//  This library is free software; you can redistribute it and/or
+//  modify it under the terms of version 2.1 of the GNU Lesser
+//  General Public License as published by the Free Software Foundation.
+//
+//  This library is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//  Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public
+//  License along with this library; if not, write to the Free Software
+//  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+//
+
+require_once dirname(__FILE__)."/../Config.php";
+require_once dirname(__FILE__)."/../ServiceTest.php";
+
+class JSONRoundtripTest extends ServiceTest {
+    public function testRoundtrip() {
+        $resp = $this->apiTestAdmin("/library/Samples/Sheboygan/Data/Parcels.FeatureSource/content.json", "GET", null);
+        $this->assertStatusCodeIs(200, $resp);
+        $this->assertContentKind($resp, Configuration::MIME_JSON);
+
+        $resp = $this->apiTestAdmin("/library/Samples/Sheboygan/Data/Parcels.FeatureSource/content.json", "POST", $resp->getContent());
+        $this->assertStatusCodeIs(201, $resp);
+    }
+}
+
+?>
