9f1cdb79313754e7f963349a670f086d2b15e73c
Jackie Ng <jumpinjackie@gmail.com>
2016-10-25 01:15:23 +1100

    #138: Port over REST services tests

---
+++ integration_test/RestServices/MoveResourceTest.php
@@ -0,0 +1,40 @@
+<?php
+
+//
+//  Copyright (C) 2016 by Jackie Ng
+//
+//  This library is free software; you can redistribute it and/or
+//  modify it under the terms of version 2.1 of the GNU Lesser
+//  General Public License as published by the Free Software Foundation.
+//
+//  This library is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//  Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public
+//  License along with this library; if not, write to the Free Software
+//  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+//
+
+require_once dirname(__FILE__)."/../ServiceTest.php";
+require_once dirname(__FILE__)."/../Config.php";
+
+class MoveResourceTest extends ServiceTest {
+    public function testOperation() {
+        $resp = $this->apiTest("/services/copyresource", "POST", array("session" => $this->adminSessionId, "source" => "Library://Samples/Sheboygan/Layers/Parcels.LayerDefinition", "destination" => "Library://RestUnitTests/Parcels.LayerDefinition", "overwrite" => 1));
+        $this->assertStatusCodeIs(200, $resp);
+        $resp = $this->apiTest("/services/moveresource", "POST", array("session" => $this->adminSessionId, "source" => "Library://RestUnitTests/Parcels.LayerDefinition", "destination" => "Library://RestUnitTests/Parcels2.LayerDefinition", "overwrite" => 1));
+        $this->assertStatusCodeIs(200, $resp);
+        $resp = $this->apiTest("/services/moveresource", "POST", array("session" => $this->anonymousSessionId, "source" => "Library://RestUnitTests/Parcels2.LayerDefinition", "destination" => "Library://RestUnitTests/Parcels3.LayerDefinition", "overwrite" => 1));
+        $this->assertStatusCodeIs(401, $resp);
+        $resp = $this->apiTest("/library/RestUnitTests/Parcels2.LayerDefinition/content.xml", "GET", array("session" => $this->anonymousSessionId));
+        $this->assertStatusCodeIs(200, $resp);
+        $this->assertMimeType(Configuration::MIME_XML, $resp);
+        $resp = $this->apiTest("/library/RestUnitTests/Parcels.LayerDefinition/content.xml", "GET", array("session" => $this->anonymousSessionId));
+        $this->assertStatusCodeIs(404, $resp);
+        $this->assertMimeType(Configuration::MIME_XML, $resp);
+    }
+}
+
+?>
