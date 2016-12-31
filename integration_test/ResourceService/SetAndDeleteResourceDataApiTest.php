c4979622467a706b0ac6e6dfeef3172a3cb42b43
Jackie Ng <jumpinjackie@gmail.com>
2016-12-31 16:39:41 +1100

    #138: Port session-based resource data API tests

--- integration_test/ResourceService/SetAndDeleteResourceDataApiTest.php
+++ integration_test/ResourceService/SetAndDeleteResourceDataApiTest.php
@@ -21,7 +21,105 @@ require_once dirname(__FILE__)."/../Config.php";
 require_once dirname(__FILE__)."/../ServiceTest.php";
 
 class SetResourceDataTest extends ServiceTest {
-    public function testBadRequests() {
+    private function getSessionResourceId() {
+        return "/session/".$this->adminSessionId."/Empty.FeatureSource";
+    }
+    public function testSessionOperation() {
+        $emptyFeatureSourceXml = '<?xml version="1.0" encoding="UTF-8"?><FeatureSource xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="FeatureSource-1.0.0.xsd"><Provider>OSGeo.SDF</Provider><Parameter><Name>File</Name><Value>%MG_DATA_FILE_PATH%Empty.sdf</Value></Parameter></FeatureSource>';
+        $resPart = $this->getSessionResourceId();
+        $args = array("type" => "File", "data" => $this->makeContentBlob("<Test></Test>"));
+
+        $resp = $this->apiTest("$resPart/content.xml", "POST", $emptyFeatureSourceXml);
+        $this->assertStatusCodeIs(201, $resp);
+
+        $resp = $this->apiTest("$resPart/data/test.xml", "POST", null);
+        $this->assertStatusCodeIs(400, $resp);
+        $this->assertMimeType(Configuration::MIME_HTML, $resp);
+
+        //Load the data item - All tests should work because the session id in the resource id is enough to provide valid credentials and is first under consideration
+        $resp = $this->apiTestWithCredentials("$resPart/data/test.xml", "POST", $args, "Foo", "Bar");
+        $this->assertStatusCodeIs(200, $resp);
+        $this->assertMimeType(Configuration::MIME_HTML, $resp);
+
+        $resp = $this->apiTestAnon("$resPart/data/test.xml", "POST", $args);
+        $this->assertStatusCodeIs(200, $resp);
+        $this->assertMimeType(Configuration::MIME_HTML, $resp);
+
+        $resp = $this->apiTestAdmin("$resPart/data/test.xml", "POST", $args);
+        $this->assertStatusCodeIs(200, $resp);
+        $this->assertMimeType(Configuration::MIME_HTML, $resp);
+
+        $resp = $this->apiTest("$resPart/data/test.xml", "POST", $args);
+        $this->assertStatusCodeIs(200, $resp);
+        $this->assertMimeType(Configuration::MIME_HTML, $resp);
+
+        //Check the data item is on the list
+        $resp = $this->apiTest("$resPart/datalist.xml", "GET", null);
+        $this->assertStatusCodeIs(200, $resp);
+        $this->assertXmlContent($resp);
+        $this->assertTrue(strpos($resp->getContent(), ">test.xml</") !== FALSE);
+
+        $resp = $this->apiTestAdmin("$resPart/datalist.xml", "GET", null);
+        $this->assertStatusCodeIs(200, $resp);
+        $this->assertXmlContent($resp);
+        $this->assertTrue(strpos($resp->getContent(), ">test.xml</") !== FALSE);
+
+        $resp = $this->apiTestAdmin("$resPart/datalist.json", "GET", null);
+        $this->assertStatusCodeIs(200, $resp);
+        $this->assertMimeType(Configuration::MIME_JSON, $resp);
+        $json = json_decode($resp->getContent());
+        $this->assertNotNull($json, "Expected valid json");
+        $bFound = false;
+        foreach ($json->ResourceDataList->ResourceData as $resData) {
+            if ($resData->Name == "test.xml") {
+                $bFound = true;
+                break;
+            }
+        }
+        $this->assertTrue($bFound);
+
+        $resp = $this->apiTest("$resPart/datalist.json", "GET", null);
+        $this->assertStatusCodeIs(200, $resp);
+        $this->assertMimeType(Configuration::MIME_JSON, $resp);
+        $json = json_decode($resp->getContent());
+        $this->assertNotNull($json, "Expected valid json");
+        $bFound = false;
+        foreach ($json->ResourceDataList->ResourceData as $resData) {
+            if ($resData->Name == "test.xml") {
+                $bFound = true;
+                break;
+            }
+        }
+        $this->assertTrue($bFound);
+
+        //Delete the item
+        $resp = $this->apiTest("$resPart/data/test.xml", "DELETE", null);
+        $this->assertStatusCodeIs(200, $resp);
+
+        //Now check the data item is no longer there
+        $resp = $this->apiTestAdmin("$resPart/datalist.xml", "GET", null);
+        $this->assertStatusCodeIs(200, $resp);
+        $this->assertTrue(strpos($resp->getContent(), ">test.xml</") === FALSE);
+
+        $resp = $this->apiTest("$resPart/datalist.xml", "GET", null);
+        $this->assertStatusCodeIs(200, $resp);
+        $this->assertTrue(strpos($resp->getContent(), ">test.xml</") === FALSE);
+
+        $resp = $this->apiTestAdmin("$resPart/datalist.json", "GET", null);
+        $this->assertStatusCodeIs(200, $resp);
+        $this->assertMimeType(Configuration::MIME_JSON, $resp);
+        $json = json_decode($resp->getContent());
+        $this->assertNotNull($json, "Expected valid json");
+        $this->assertFalse(isset($json->ResourceDataList->ResourceData), "Expected no ResourceData");
+
+        $resp = $this->apiTest("$resPart/datalist.json", "GET", null);
+        $this->assertStatusCodeIs(200, $resp);
+        $this->assertMimeType(Configuration::MIME_JSON, $resp);
+        $json = json_decode($resp->getContent());
+        $this->assertNotNull($json, "Expected valid json");
+        $this->assertFalse(isset($json->ResourceDataList->ResourceData), "Expected no ResourceData");
+    }
+    public function testLibraryBadRequests() {
         $args = array("type" => "File", "data" => $this->makeContentBlob("<Test></Test>"));
         
         $resp = $this->apiTestWithCredentials("/library/Samples/Sheboygan/Data/Parcels.FeatureSource/data/test.xml", "POST", $args, "Foo", "Bar");
@@ -36,7 +134,7 @@ class SetResourceDataTest extends ServiceTest {
         $this->assertStatusCodeIs(400, $resp);
         $this->assertMimeType(Configuration::MIME_HTML, $resp);
     }
-    public function testOperation() {
+    public function testLibraryOperation() {
         $args = array("type" => "File", "data" => $this->makeContentBlob("<Test></Test>"));
         //Load the data item
         $mergedArgs = array_merge($args, array("session" => $this->anonymousSessionId));
