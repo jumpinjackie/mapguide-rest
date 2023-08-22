1aba11155e6bae0de9de06b17f1fc1a9404e8210
Jackie Ng <jumpinjackie@gmail.com>
2023-08-22 20:52:45 +1000

    Show info banner on integration test bootstrap. Also remove MG_WEBCONFIG_INI_PATH from env setup scripts

--- env_setup.bat
+++ env_setup.bat
@@ -6,13 +6,11 @@ REM Modify the variables below to match your specific PHP install
 
 REM SET PHPRC=C:\mg-312-install\Web\Php\php.ini
 REM SET PATH=%PATH%;C:\mg-312-install\Web\Php
-REM SET MG_WEBCONFIG_INI_PATH=C:\mg-312-install\Web\www\webconfig.ini
 REM SET COMPOSER=composer.php5.json
 REM SET MG_REST_ROOT_URL=http://localhost:8018/mapguide/rest
 
 SET PHPRC=C:\mg-4.0-install\Web\Php\php.ini
 SET PATH=%PATH%;C:\mg-4.0-install\Web\Php
-SET MG_WEBCONFIG_INI_PATH=C:\mg-4.0-install\Web\www\webconfig.ini
 SET MG_REST_ROOT_URL=http://localhost:8018/mapguide/rest
 
 echo PHP environment set using php.ini from %PHPRC%
