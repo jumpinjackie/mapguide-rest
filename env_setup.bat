a07c61594f62ec9ee43bb6971a1860378530ad86
Jackie Ng <jumpinjackie@gmail.com>
2023-08-22 19:05:18 +1000

    Add helper batch script to init the mapguide-rest environment with the right PHP installation

---
+++ env_setup.bat
@@ -0,0 +1,10 @@
+REM env_setup.bat
+REM
+REM This batch file initializes the mapguide-rest dev environemnt to match your version/installation of PHP
+REM Modify the variables below to match your specific PHP install
+
+REM SET PHPRC=C:\mg-312-install\Web\Php\php.ini
+REM SET PATH=%PATH%;C:\mg-312-install\Web\Php
+
+SET PHPRC=C:\mg-4.0-install\Web\Php\php.ini
+SET PATH=%PATH%;C:\mg-4.0-install\Web\Php
