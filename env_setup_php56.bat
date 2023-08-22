647743c32bb7cd5938313b7c30e08f9aa32f2d63
Jackie Ng <jumpinjackie@gmail.com>
2023-08-22 19:18:45 +1000

    Add 5.6 copy of env setup. Update appveyor pipeline to download the correct composer version.

--- env_setup_php56.bat
+++ env_setup_php56.bat
@@ -4,11 +4,11 @@ REM
 REM This batch file initializes the mapguide-rest dev environemnt to match your version/installation of PHP
 REM Modify the variables below to match your specific PHP install
 
-REM SET PHPRC=C:\mg-312-install\Web\Php\php.ini
-REM SET PATH=%PATH%;C:\mg-312-install\Web\Php
-REM SET COMPOSER=composer.php5.json
+SET PHPRC=C:\mg-312-install\Web\Php\php.ini
+SET PATH=%PATH%;C:\mg-312-install\Web\Php
+SET COMPOSER=composer.php5.json
 
-SET PHPRC=C:\mg-4.0-install\Web\Php\php.ini
-SET PATH=%PATH%;C:\mg-4.0-install\Web\Php
+REM SET PHPRC=C:\mg-4.0-install\Web\Php\php.ini
+REM SET PATH=%PATH%;C:\mg-4.0-install\Web\Php
 
 echo PHP environment set using php.ini from %PHPRC%
